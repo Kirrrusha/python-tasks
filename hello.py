e5e8e2531ef1de780525c53f5e428c7d93fe8830
Kirill Lebedenko <kialebedenko@cloud.ru>
2025-11-23 22:16:13 +0300

    task-1

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main():
+    name = input("Введите ваше имя: ")
+
+    age = input("Введите ваш возраст: ")
+
+    print(f"Привет, {name}! Тебе {age} лет.")
+
+if __name__ == "__main__":
+    main()
